c0813d0cd61984ceb81cca7b8cae7cce10933385
Haryodele <lizzyayodele.ay@gmail.com>
2023-06-16 18:55:22 -0700

    Create DAY 1.py
    DAY ONE

---
+++ DAY 1.py
@@ -0,0 +1,37 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+name = "Jack"
+print(name)
+name = "Angela"
+print(name)
+print(len(input("What is your name?")))
+name = input("What is your name?")
+length = len(name)
+print(length)
+a = input("a:")
+b = input("b:")
+c = a
+a = b
+b = c
+print("a = " + a)
+print("b = " + b)
+print("Welcome to the band name generator.")
+city = input("Which city did you grow up in?")
+print(city)
+pet = input("What is the name of your pet?")
+print("Your band name could be"  + city + " "  + pet)
+print(f"Your band name could be city ''  pet")
